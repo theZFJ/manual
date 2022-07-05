3b603b7b2c98ee3dfbd7f724d6cb99d1ba709f76
akirarorschach <69493313+akirarorschach@users.noreply.github.com>
2022-07-04 21:52:15 -0400

    remove template info

--- docs/conf.py
+++ docs/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'The Zombie Fridge Journal Manual'
+copyright = '2022, The Zombie Fridge Journal'
+author = 'The Zombie Fridge Journal'
 
 release = '0.1'
 version = '0.1.0'
