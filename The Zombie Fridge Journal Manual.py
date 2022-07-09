883ebb5b4f1493e5a6926b56c8163d8ad34722ac
akirarorschach <69493313+akirarorschach@users.noreply.github.com>
2022-07-08 22:35:51 -0400

    Add version
    per #2

--- The Zombie Fridge Journal Manual.py
+++ The Zombie Fridge Journal Manual.py
@@ -2,6 +2,8 @@
 The Zombie Fridge Journal Manual - Style guide and other useful information for ZFJ journalists.
 '''
 
+__version__ = "0.1.0"
+
 def manual():
     '''
     Style guide and other useful information for ZFJ journalists.
